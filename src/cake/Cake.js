bf0d6e290d01ca03012dfa4e8cba53e049494e0c
blezzydcruz <mbannedelacruz@hotmail.com>
2023-09-04 15:09:52 +0100

    completed mvp

---
+++ src/cake/Cake.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+const Cake = ({ cakeName, ingredients, price, rating }) => {
+  return (
+    <>
+        <h2>{cakeName}</h2>
+        <ul>
+          {ingredients.map((ingredients, index) => (
+            <li key={index}>{ingredients}</li>
+          ))}
+        </ul>
+        <p>Price: {price}</p>
+        <p>Rating: {rating}</p>
+
+    </>
+  )
+}
+
+export default Cake;
